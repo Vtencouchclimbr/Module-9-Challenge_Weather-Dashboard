f049410217581df1a4a3c0620b6328d956a99651
Vtencouchclimbr <148500829+Vtencouchclimbr@users.noreply.github.com>
2024-08-09 17:19:54 -0600

    add weather routing functions

--- Develop/server/src/routes/api/weatherRoutes.ts
+++ Develop/server/src/routes/api/weatherRoutes.ts
@@ -1,19 +1,57 @@
 import { Router } from 'express';
 const router = Router();
 
-// import HistoryService from '../../service/historyService.js';
-// import WeatherService from '../../service/weatherService.js';
+import HistoryService from '../../service/historyService.js';
+import WeatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // TODO: GET weather data from city name
   // TODO: save city to search history
+  const { cityName } = req.body;
+
+  if (!cityName) {
+    return res.status(400).json({ error: 'City name is required'});
+  }
+  try {
+    const weatherData = await WeatherService.getWeatherByCityName(cityName);
+
+    const cityWithId = await HistoryService.saveCity(cityName);
+
+    res.json({ city: cityWithId, weather: weatherData });
+  } catch (error) {
+    console.error('Error processing request:', error);
+    res.status(500).json({ error: 'Failed to retrieve weather data or save city to history'});
+  }
 });
 
 // TODO: GET search history
-router.get('/history', async (req, res) => {});
+router.get('/history', async (req, res) => {
+  try {
+    const history = await HistoryService.getHistory();
+    res.json(history);
+  } catch (error) {
+    console.error('Error retrieving history:', error);
+    res.status(500).json({ error: 'Failed to retrieve search history '});
+  }
+});
 
 // * BONUS TODO: DELETE city from search history
-router.delete('/history/:id', async (req, res) => {});
+router.delete('/history/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedCity = await HistoryService.deleteCityById(id);
+
+    if (deletedCity) {
+      res.json({ message: 'City deleted from history', city: deletedCity });
+    } else {
+      res.status(404).json({ error: 'City not found' });
+    }
+  } catch (error) {
+    console.error('Error deleting city:', error);
+    res.status(500).json({ error: 'Failed to delete city from the search history' });
+  }
+});
 
 export default router;
